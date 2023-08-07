072d947cfe193b41d1dd7750f4ffaf8f517634eb
Anton <antoha_denisenkov@yandex.ru>
2023-08-07 10:15:05 +0500

    fix lint

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -3,7 +3,7 @@ import styles from './loader.module.css';
 export const Loader = () => {
   return (
     <div className={styles.loaderBlock}>
-      <div className={styles.loader}></div>;
+      <div className={styles.loader}></div>
     </div>
   );
 };
